714e0c1e4243e49eedb1254e35e4b5e0e3c754f3
kumanoryo <r-kumano@groovenauts.jp>
2023-03-31 21:36:16 +0900

    GPGの利用をやめるので設定を削除した

--- docker/config.fish
+++ docker/config.fish
@@ -2,7 +2,6 @@ set -g theme_date_timezone UTC
 set -g theme_date_format "+%F %H:%M:%S (%Z)"
 set -g theme_color_scheme solarized
 set -g theme_newline_cursor yes
-set -gx GPG_TTY (tty)
 
 set fish_color_command brpurple
 
