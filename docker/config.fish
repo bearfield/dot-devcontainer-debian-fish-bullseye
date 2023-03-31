e9528136aae5a891b60fd770864f2d7104d2c560
kumanoryo <r-kumano@groovenauts.jp>
2023-03-31 21:14:56 +0900

    git commit時に使用するgpgに必要なfishの環境変数を追加した。

--- docker/config.fish
+++ docker/config.fish
@@ -2,6 +2,7 @@ set -g theme_date_timezone UTC
 set -g theme_date_format "+%F %H:%M:%S (%Z)"
 set -g theme_color_scheme solarized
 set -g theme_newline_cursor yes
+set -gx GPG_TTY (tty)
 
 set fish_color_command brpurple
 
